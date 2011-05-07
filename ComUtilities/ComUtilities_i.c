74a94ca5c232f7a0033c066385859f67c106c554
Maximilian Corrientes <max@rockplaces.com>
2011-05-07 02:05:39 +0100

    ComUtilities changed and changed the "Debug Any Output" links to "Active"

--- ComUtilities/ComUtilities_i.c
+++ ComUtilities/ComUtilities_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Jul 27 21:19:46 2010
+/* at Sat May 07 01:59:10 2011
  */
 /* Compiler settings for ComUtilities.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
